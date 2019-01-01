54314ed90b52cdef82a79d3d973822213d8c5f3c
AliRaish <100alialiraishali@gmail.com>
2019-01-01 21:56:44 +0500

    Add shop.js

---
+++ commands/shop.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch');
+const Canvas = require('canvas');
+const Discord = require('discord.js');
+const snekfetch = require('snekfetch');
+
+exports.run = async (client, message) => { // eslint-disable-line no-unused-vars
+    const link = 'https://fortnite-public-api.theapinetwork.com/prod09/store/get';
+    fetch(link).then(result => result.json()).then(async res => {
+        const canvas = Canvas.createCanvas(800, 600);
+        const ctx = canvas.getContext('2d');
+        
+        let num = 0;
+        for (var i = 0; i < (Math.round(res.items.length / 4)); i++) {
+            for (var j = 0; j < 4; j++) {
+                if (num < res.items.length) {
+                    const { body: buffer } = await snekfetch.get(res.items[num].item.images.information);
+                    const avatar = await Canvas.loadImage(buffer);
+                    ctx.drawImage(avatar, j * 200, i * 200, 200, 200);
+                    num++;
+                }
+            }
+        }
+        const attachment = new Discord.Attachment(canvas.toBuffer(), 'shop.png');
+        message.channel.send(`Shop data for **${res.date}**`, attachment);
+    }).catch(err => console.error(err));
+};
